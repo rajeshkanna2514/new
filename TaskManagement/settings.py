921be024d12db8ba11f6688dc2a7aa129bcb031f
rajeshkanna2514 <rajeshkanna5533@gmail.com>
2024-07-30 13:03:06 +0530

    changes

--- TaskManagement/settings.py
+++ TaskManagement/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -97,11 +98,16 @@ WSGI_APPLICATION = 'TaskManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TaskManagement',
+        'USER': 'postgres',
+        'PASSWORD': 'post',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
+    },
 }
 
 
@@ -140,6 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
